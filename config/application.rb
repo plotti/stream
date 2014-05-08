d14865a05e1bbc2e1df3b7e27df178f3a5e23b10
Thomas Plotkowiak <plotti@gxm.net>
2014-05-08 11:50:09 +0200

    login page

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,9 @@ module Stream
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
+    #devise locale
+    config.i18n.default_locale = :de
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Stream::Application.routes.draw do
 
   devise_for :users
   resources :users, :only => [:index, :approve, :block]
+  match '/welcome' => "pages#welcome"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -15,6 +16,7 @@ Stream::Application.routes.draw do
       get "favorite"
     end
   end
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -61,7 +63,10 @@ Stream::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'songs#index'
+  authenticated :user do
+    root :to => "songs#index"
+  end
+  root :to => 'pages#welcome'
 
   # See how all your routes lay out with "rake routes"
 
