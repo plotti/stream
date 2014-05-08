02fe220057025af664b68a5d6546b04ba462e699
server <plotti@gmx.net>
2014-05-09 00:25:18 +0200

    fix for listener

--- config/initializers/listen.rb
+++ config/initializers/listen.rb
@@ -1,18 +1,24 @@
 MP3_PATH = YAML.load_file("#{Rails.root}/config/config.yml")["mp3_path"]
 
 listener = Listen.to(MP3_PATH, latency: 0.5) do |modified, added, removed|
-	if added != [] && added.first.include?("incomplete")
-	 	s = Song.where(:filename => added.first).first
-	 	if s == nil
-	 		s = Song.create(:filename => added.first)
-	 	end
-	 	s.play(added.first)
-	elsif added != [] && !added.first.include?("incomplete")
-		data = added.first.split("/").last.gsub(".mp3", "").split(" - ")
-		s = Song.where(:artist => data.first).where(:track => data.last).first
-		puts "TRIGGERED EVENT FOR MOVE DATA: #{data}. SONG #{s}"
-		if s != nil
-			s.move_to_archive(added.first)
+	puts "GENERAL ADDED: #{added}"
+	puts "GENERAL MODIFIED #{modified}"
+	puts "GENERAL REMOVED #{removed}"
+	if added != []
+		added.each do |added_file|
+		if added_file.include?("incomplete")
+	 		s = Song.where(:filename => added_file).first
+	 		if s == nil
+	 			s = Song.create(:filename => added_file)
+	 		end
+	 		s.play(added_file)
+		elsif !added_file.include?("incomplete")
+			data = added_file.split("/").last.gsub(".mp3", "").split(" - ")
+			s = Song.where(:artist => data.first).where(:track => data.last).first
+			if s != nil
+				s.move_to_archive(added_file)
+			end
+		end
 		end
 	end
 end
