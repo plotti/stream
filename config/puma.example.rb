a7119d1e0b6b8d500b268bc6bfdf0f96845ac0c1
Thomas Plotkowiak <plotti@gxm.net>
2014-11-17 17:03:52 +0100

    fix

--- config/puma.example.rb
+++ config/puma.example.rb
@@ -72,7 +72,7 @@ threads 0, 16
 # The default is “tcp://0.0.0.0:9292”.
 #
 # bind 'tcp://0.0.0.0:9292'
-bind 'unix:/tmp/puma.reradio.sock'
+bind 'unix:///tmp/puma.reradio.sock'
 # bind 'unix:///var/run/puma.sock?umask=0777'
 # bind 'ssl://127.0.0.1:9292?key=path_to_key&cert=path_to_cert'
 
